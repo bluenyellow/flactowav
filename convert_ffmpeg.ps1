35b3a8acd9131059abc8a2b9b46125b3cca0b4ba
bluenyellow <reply77@protonmail.ch>
2022-04-20 20:39:18 +0000

    Add files via upload

---
+++ convert_ffmpeg.ps1
@@ -0,0 +1,17 @@
+﻿$foldre=Get-ChildItem -Recurse -Force | Where {$_.Extension -eq '.flac'}
+
+foreach($item in $foldre){
+
+
+$menoi=$item.Name
+
+$meno=$item.BaseName
+$out= -join("$meno", ".", "wav")
+
+
+
+ffmpeg -i $menoi $out
+
+Remove-Item $menoi
+
+}
